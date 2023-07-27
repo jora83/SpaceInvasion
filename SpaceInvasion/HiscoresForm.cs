9777abf01b0966df940ab19992049a69d8c96178
alex jora <joraalex13@gmail.com>
2023-07-28 00:20:59 +0300

    displaying in the highscore form

--- SpaceInvasion/HiscoresForm.cs
+++ SpaceInvasion/HiscoresForm.cs
@@ -8,20 +8,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Data;
 namespace SpaceInvasion
 {
     public partial class HiscoresForm : Form
     {
         HighscoreSystem HighscoreSystem = new HighscoreSystem();
         List<KeyValuePair<string, int>> highscores = new List<KeyValuePair<string, int>>();
+        DataTable dataTable = new DataTable();
 
         public HiscoresForm()
         {
             InitializeComponent();
             HighscoreSystem.LoadHighscores();
             highscores = HighscoreSystem.GetHighscores();
-            ShowHighscores();
+            //ShowHighscores();
+            ConvertToDataTable();
+        }
+
+        public void ConvertToDataTable()
+        {
+            dataTable.Columns.Clear();
+            dataTable.Columns.Add("Place", typeof(int));
+            dataTable.Columns.Add("Username", typeof(string));
+            dataTable.Columns.Add("Score", typeof(int));
+
+            int place = 1;
+            foreach(var kvp in highscores)
+            { 
+                dataTable.Rows.Add(place, kvp.Key, kvp.Value);
+                place++;
+            }
+
+            dataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dataGridView.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dataGridView.DataSource = dataTable;
         }
 
         private void ShowHighscores()
